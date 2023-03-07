198c4d7350996ce8c2d7f0154747760d39ebe503
Isanin Sergey <sergey.isanin@gmail.com>
2023-03-07 11:36:14 +0300

    Revert "test uppdate"
    This reverts commit 84167f5225886251c3251b6486c9fad97e6834eb.

--- src/main.js
+++ src/main.js
@@ -1,31 +1,8 @@
-import PortalVue from "portal-vue";
-import { createApp } from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
-
-import { createPopper } from "@popperjs/core";
-const popcorn = document.querySelector("#popcorn");
-const tooltip = document.querySelector("#tooltip");
-createPopper(popcorn, tooltip, {
-  placement: "top"
-});
-
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App)
-}).$mount("#app");
-
-const app = createApp(App);
-app.use(PortalVue);
-app.mount("#app");
+  render: h => h(App),
+}).$mount('#app')
